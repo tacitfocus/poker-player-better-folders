913c0345735a3d98f963446885548b649af1c322
Sam Livingston-Gray <geeksam@gmail.com>
2022-03-01 11:15:04 -0800

    Log game state

--- player.rb
+++ player.rb
@@ -1,13 +1,21 @@
+require 'logger'
 
 class Player
 
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
+    logger.info game_state.to_s
     1_000 - 5
   end
 
   def showdown(game_state)
 
+  end
+
+  private
+
+  def logger
+    @_logger ||= Logger.new($stderr)
   end
 end
