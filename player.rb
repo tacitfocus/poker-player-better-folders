c89f6aa8b22f956f29d8dc1c6a7c9cd081d09e09
Sam Livingston-Gray <geeksam@gmail.com>
2022-03-01 11:21:45 -0800

    always bet what we [think we] actually have

--- player.rb
+++ player.rb
@@ -5,8 +5,11 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    logger.info game_state.to_s
-    1_000 - 5
+    in_action_id = game_state["in_action"]
+    our_player = players.detect { |e| e["id"] == in_action_id }
+    fat_stack = our_player["stack"].to_i
+
+    return fat_stack # all in, baby
   end
 
   def showdown(game_state)
@@ -18,4 +21,24 @@ class Player
   def logger
     @_logger ||= Logger.new($stderr)
   end
+
+  # def chen_score(c1, c2)
+  #   _score = [ c1, c2 ].map { |e| rv(e) }.max
+  # end
+
+=begin
+function Chen(C 1 , C 2 )
+    score := Max(rv(C 1 ), rv(C 2 ))
+    if Csuit(C 1 ) == Csuit(C 2 ) then score := score + 2
+    end if
+
+    gap := |Cvalue(C 1 ) == Cvalue(C 2 )|
+    if gap = 0 then score := score×2
+    else if gap = 1 then score := score+1
+    else if gap = 2 then score := score-1
+    else if gap = 3 then score := score-2
+    else if gap = 4 then score := score-4
+    else score := score – 5 end if
+return score / 20
+=end
 end
